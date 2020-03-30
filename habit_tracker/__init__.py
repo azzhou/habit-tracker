60d814cada0a766dc27ad1414701958b2e126110
Andy <zhouzandy@gmail.com>
2020-03-30 15:26:00 -0700

    Create application factory function

--- habit_tracker/__init__.py
+++ habit_tracker/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
-from flask_mongoengine import MonoEngine
-from flaskblog.config import DevConfig
+from flask_mongoengine import MongoEngine
+from habit_tracker.config import DevConfig
 
 
-db = MonoEngine()
+db = MongoEngine()
 
 
 def create_app(config_class=DevConfig):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_class)
-    app.config.from_pyfile('private_config.cfg', silent=True)
+    # app.config.from_pyfile('private_config.cfg', silent=True)
 
     db.init_app(app)
+
+    # Import blueprint objects after db setup
+    from habit_tracker.main.routes import main  # noqa 402
+    app.register_blueprint(main)
 
     return app
