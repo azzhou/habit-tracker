e5cedf14c52b17b04cf63ef3189821e7eb6df85b
Andy <zhouzandy@gmail.com>
2020-04-01 15:43:30 -0700

    Add daily checklist for logged in users
    Add html template
    Update home page route to redirect to the checklist if logged in

--- habit_tracker/main/routes.py
+++ habit_tracker/main/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
 
 
 main = Blueprint("main", __name__)
@@ -7,4 +8,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @main.route("/home/")
 def home():
-    return render_template("home.html")
+    if current_user.is_authenticated:
+        return redirect(url_for("habits.habit_list"))
+    else:
+        return render_template("home.html")
