34a700a5d3786783f710d648c1c1fff2f1b08be4
Andy <zhouzandy@gmail.com>
2020-04-10 14:37:03 -0700

    Allow all checkboxes on my_habits to be used
    Replace the daily checklist form that only allowed changes to current
    day's tasks
    Each checkbox redirects to habit update route to mark as
    complete/incomplete

--- habit_tracker/habits/forms.py
+++ habit_tracker/habits/forms.py
@@ -1,29 +1,15 @@
-from flask_wtf import FlaskForm  # noqa 902
+from flask_wtf import FlaskForm
 from flask_login import current_user
 from habit_tracker.documents import Habit
-from wtforms import BooleanField, SubmitField, StringField, IntegerField
+from wtforms import SubmitField, StringField, IntegerField
 from wtforms.validators import DataRequired, Length, ValidationError, NumberRange
-from datetime import date
-
-
-def create_daily_habits_form(habits):
-    class DailyHabitsForm(FlaskForm):
-        submit = SubmitField("Update")
-    for habit in habits:
-        setattr(
-            DailyHabitsForm,
-            habit.name,
-            BooleanField(habit.name, default=habit.is_complete(date.today()))
-        )
-    form = DailyHabitsForm()
-    return form
 
 
 class AddHabitForm(FlaskForm):
     # inactive_habits = Habit.objects(user=current_user.id, active=False)
     name = StringField("Name", validators=[
         DataRequired(),
-        Length(min=1, max=50, message="Name must be between 1 and 50 characters long.")
+        Length(min=1, max=30, message="Name must be between 1 and 30 characters long.")
     ])
     points = IntegerField("Points", validators=[NumberRange(min=1, max=5)], default=3)
     submit = SubmitField("Add Habit")
