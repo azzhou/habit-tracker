19757eb504bd4278d6b6c77084e6333bc7e2c9cf
Andy <zhouzandy@gmail.com>
2020-04-10 20:32:28 -0700

    Remove points field for habits

--- habit_tracker/habits/forms.py
+++ habit_tracker/habits/forms.py
@@ -1,20 +1,19 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from habit_tracker.documents import Habit
-from wtforms import SubmitField, StringField, IntegerField
-from wtforms.validators import DataRequired, Length, ValidationError, NumberRange
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class AddHabitForm(FlaskForm):
     # inactive_habits = Habit.objects(user=current_user.id, active=False)
     name = StringField("Name", validators=[
         DataRequired(),
-        Length(min=1, max=30, message="Name must be between 1 and 30 characters long.")
+        Length(min=1, max=30, message="Habit name must be between 1 and 30 characters long.")
     ])
-    points = IntegerField("Points", validators=[NumberRange(min=1, max=5)], default=3)
     submit = SubmitField("Add Habit")
 
     # User must not already have another habit with the same name
     def validate_name(self, name):
         if Habit.objects(user=current_user.id, name=name.data):
-            raise ValidationError("You have previously added that habit.")
+            raise ValidationError("You are already tracking that habit.")
