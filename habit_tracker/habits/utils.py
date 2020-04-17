c1b60fe0a994b8b88b8f86590055e36a7707e3ac
Andy <zhouzandy@gmail.com>
2020-04-17 13:35:28 -0700

    Update functions that create checklist and grid
    Change functions used for creating checklist and grid to be helpers that
    feed into a single function for each.
    Flesh out habit grid functionality to calculate levels of completion
    used in color classification.

--- habit_tracker/habits/utils.py
+++ habit_tracker/habits/utils.py
@@ -1,72 +1,154 @@
-from datetime import date, timedelta
+from bisect import bisect
 from calendar import month_abbr
+from collections import namedtuple
+from datetime import date, timedelta
 from flask import url_for
 
 
 def date_range(start_date, end_date, reverse=False):
-    for n in range(int((end_date - start_date).days) + 1):
+    """Generator that yields datetime.date objects between specified start and end dates.
+
+    Args:
+        start_date (datetime.date): Inclusive start date of the range.
+        end_date (datetime.date): Inclusive end date of the range.
+        reverse (bool, optional): Whether dates should be yielded from end to start. Defaults to False.
+
+    Yields:
+        datetime.date: Current date in the range that is yielded.
+    """
+    for n in range((end_date - start_date).days + 1):
         yield start_date + timedelta(n) if not reverse else end_date - timedelta(n)
 
 
 def _grid_start_date(end_date):
-    """
-    Returns the date of the first date shown in the habits history grid
-    i.e. the Sunday representing the top-left corner of the grid
+    """Calculates the first date shown in the habit history grid.
+
+    Only meant for use in the `create_habit_history_grid` function.
+
+    Args:
+        end_date (datetime.date): Inclusive end date that is shown in the habit history grid.
+
+    Returns:
+        datetime: The date of the first Sunday shown in the top-left corner of the habit history grid.
     """
     last_sunday = (
         end_date
         if end_date.weekday() == 6  # date is Sunday
         else end_date - timedelta(days=end_date.weekday() + 1)
     )
-    return last_sunday - timedelta(weeks=52)
+    first_sunday = last_sunday - timedelta(weeks=52)
+    return first_sunday
 
 
-def grid_month_labels(end_date=date.today()):
+def _grid_month_labels(start_date, end_date):
+    """Creates a list of month labels for the habit history grid.
+
+    Only meant for use in the `create_habit_history_grid` function.
+
+    Args:
+        start_date (datetime.date): Inclusive start date that is shown in the habit history grid.
+        end_date (datetime.date): Inclusive end date that is shown in the habit history grid.
+
+    Returns:
+        list[str]: List of labels for each column in the grid. Only list positions referring to
+                   columns (weeks) of the grid that contain the first of a month have a
+                   month abbreviation, and the rest are empty.
     """
-    This generates a list of month labels for the habits history grid.
-    Month labels line up with the columns that contain the months' first days.
-    """
-    start_date = _grid_start_date(end_date)
     labels = []
-    saturday_date = start_date + timedelta(days=6)  # end of first week
-    while saturday_date <= end_date:
-        # Add abbr for month if start of month occurs during this week
+    end_of_week = start_date + timedelta(days=6)
+    while end_of_week <= end_date:
+        # Add abbreviation for month if start of month occurs during this week
         labels.append(
-            month_abbr[saturday_date.month]
-            if saturday_date.day <= 7
+            month_abbr[end_of_week.month]
+            if end_of_week.day <= 7
             else ""
         )
-        saturday_date += timedelta(weeks=1)
+        end_of_week += timedelta(weeks=1)
 
     return labels
 
 
-def grid_date_labels(end_date=date.today()):
+def _grid_squares(habits, break_points, start_date, end_date):
+    """Calculate values needed to render squares in the habit history grid.
+
+    Only meant for use in the `create_habit_history_grid` function.
+
+    Args:
+        habits (iterable): Habit document objects used in the grid.
+        break_points (list[int]): Break points used to divide up habit completion rates into levels.
+        start_date (datetime.date): Inclusive start date that is shown in the habit history grid.
+        end_date (datetime.date): Inclusive end date that is shown in the habit history grid.
+
+    Returns:
+        list[GridSquare]: List of namedtuples that have values used in the habit history grid.
     """
-    Returns list of formatted dates for one-year habit history grid.
-    These labels show up in tooltips when hovering over squares in the grid.
+    GridSquare = namedtuple("GridSquare", ["num_complete", "num_active", "level", "date_label"])
+
+    grid = []
+    curr_date = start_date
+    completion_per_habit = list(map(lambda habit: habit.is_complete_range(start_date, end_date), habits))
+    completion_per_day = zip(*completion_per_habit)
+
+    for one_day_completion in completion_per_day:
+        num_complete = sum(one_day_completion)
+        num_active = len(one_day_completion)
+        ratio = num_complete / num_active if num_active > 0 else 0
+        level = bisect(break_points, ratio) - 1
+        date_label = curr_date.strftime("%b %-d, %Y")
+        curr_date += timedelta(1)
+        grid.append(GridSquare(
+            num_complete=num_complete,
+            num_active=num_active,
+            level=level,
+            date_label=date_label)
+        )
+    return grid
+
+
+def create_habit_history_grid(habits, break_points, end_date=date.today()):
+    """Produce values and labels necessary to render a habit history grid for a user.
+
+    Args:
+        habits (iterable): Habit document objects used in the grid.
+        break_points (list[int]): Break points used to divide up habit completion rates into levels.
+        end_date (datetime.date): Inclusive end date that is shown in the habit history grid.
+
+    Returns:
+        Grid: namedtuple that includes month labels and a list of GridSquare namedtuples.
     """
     start_date = _grid_start_date(end_date)
-    return [d.strftime("%b %-d, %Y") for d in date_range(start_date, end_date, reverse=True)]
+    month_labels = _grid_month_labels(start_date, end_date)
+    squares = _grid_squares(habits, break_points, start_date, end_date)
+    Grid = namedtuple("Grid", ["month_labels", "squares"])
+    return Grid(month_labels=month_labels, squares=squares)
 
 
-def _checklist_start_date(num_days, end_date):
-    return end_date - timedelta(num_days - 1)
+def create_habit_checklist(habits, num_days, end_date=date.today()):
+    """Produce the pieces necessary to render a habit checklist.
 
+    Args:
+        habits (iterable): Habit document objects used in the checklist.
+        num_days (int): The number of days that are shown in the checklist.
+        end_date (datetime.date, optional): Last day shown in the checklist. Defaults to date.today().
 
-def checklist_date_labels(num_days, end_date=date.today()):
-    start_date = _checklist_start_date(num_days, end_date)
-    return [date.strftime("%-m/%-d") for date in date_range(start_date, end_date, reverse=True)]
-
-
-def checklist_default_values(habits, num_days, end_date=date.today()):
-    start_date = _checklist_start_date(num_days, end_date)
-    return {habit.id: [habit.is_complete(date) for date in date_range(start_date, end_date, reverse=True)]
-            for habit in habits}
-
-
-def checklist_routes(habits, num_days, end_date=date.today()):
-    start_date = _checklist_start_date(num_days, end_date)
-    return {habit.id: [url_for("habits.update_habit", slug=habit.slug, date=str(date))
-                       for date in date_range(start_date, end_date, reverse=True)]
-            for habit in habits}
+    Returns:
+        HabitChecklist: namedtuple with dictionaries of labels, values, and routes used in checklist.
+    """
+    start_date = end_date - timedelta(num_days - 1)
+    date_labels = [date.strftime("%-m/%-d") for date in date_range(start_date, end_date, reverse=True)]
+    default_values = {
+        habit.id: [
+            habit.is_complete(date)
+            for date in date_range(start_date, end_date, reverse=True)
+        ]
+        for habit in habits
+    }
+    routes = {
+        habit.id: [
+            url_for("habits.update_habit", slug=habit.slug, date=str(date))
+            for date in date_range(start_date, end_date, reverse=True)
+        ]
+        for habit in habits
+    }
+    HabitChecklist = namedtuple("HabitChecklist", ["date_labels", "default_values", "routes"])
+    return HabitChecklist(date_labels=date_labels, default_values=default_values, routes=routes)
