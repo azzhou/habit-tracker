137ae176dedb6870ed5bc166b5ce6420d310ec18
Andy <zhouzandy@gmail.com>
2020-04-23 10:56:37 -0700

    Add account page to update user info
    New html file and route
    Form for updating username/email + form for updating password
    Update navbar to link to account page

--- habit_tracker/users/forms.py
+++ habit_tracker/users/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from habit_tracker import bcrypt
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import (DataRequired, Length, Email, EqualTo,
                                 Regexp, ValidationError)
@@ -34,3 +36,40 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+
+class UpdateBasicInfoForm(FlaskForm):
+    username = StringField("Username", validators=[
+        DataRequired(),
+        Length(min=3, max=20, message="Username must be between 3 and 20 characters long."),
+        Regexp(regex="^\\w+$", message="Username must contain alphanumeric characters only.")
+    ])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit_info = SubmitField("Update")
+
+    # In-line validators to ensure fields aren't already taken
+    def validate_username(self, username):
+        if current_user.username != username.data and User.objects(username=username.data):
+            raise ValidationError("That username is already taken.")
+
+    def validate_email(self, email):
+        if current_user.email != email.data and User.objects(email=email.data):
+            raise ValidationError("That email is already taken.")
+
+
+class UpdateAccountPasswordForm(FlaskForm):
+    current_password = PasswordField("Current Password", validators=[DataRequired()])
+    new_password = PasswordField("New Password", validators=[DataRequired()])
+    confirm_new_password = PasswordField("Confirm New Password", validators=[
+        DataRequired(),
+        EqualTo("new_password", message="New password doesn't match.")
+    ])
+    submit_password = SubmitField("Change Password")
+
+    def validate_current_password(self, current_password):
+        if not bcrypt.check_password_hash(current_user.password, current_password.data):
+            raise ValidationError("Current password is incorrect.")
+
+    def validate_new_password(self, new_password):
+        if bcrypt.check_password_hash(current_user.password, new_password.data):
+            raise ValidationError("New password cannot be the same as your current password.")
--- habit_tracker/users/routes.py
+++ habit_tracker/users/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from habit_tracker.users.forms import RegistrationForm, LoginForm
-from flask_login import current_user, login_user, logout_user
+from habit_tracker.users.forms import (RegistrationForm, LoginForm,
+                                       UpdateBasicInfoForm, UpdateAccountPasswordForm)
+from flask_login import current_user, login_user, logout_user, login_required
 from habit_tracker import bcrypt
 from habit_tracker.documents import User
 
@@ -39,7 +40,7 @@ def login():
         user = User.objects(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            flash("You have logged in!", category="info")
+            flash(f"Welcome back, {user.username}!", category="info")
             return redirect(url_for('main.home'))
         else:
             flash("Login unsuccessful. Please check email and password.", category="danger")
@@ -50,3 +51,44 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("users.login"))
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    basic_info_form = UpdateBasicInfoForm()
+    password_form = UpdateAccountPasswordForm()
+    anchor = ""
+
+    # Check submit field's data because is_submitted() doesn't differentiate between forms
+    if basic_info_form.submit_info.data:
+        if basic_info_form.validate():
+            current_user.username = basic_info_form.username.data
+            current_user.email = basic_info_form.email.data
+            current_user.save()
+            flash("Your basic info has been updated!", category="success")
+            return redirect(url_for("users.account"))
+        else:
+            # Used to bring form into view using JS if there are validation errors
+            anchor = "update-basic-info"
+    else:
+        basic_info_form.username.data = current_user.username
+        basic_info_form.email.data = current_user.email
+
+    # Check submit field's data because is_submitted() doesn't differentiate between forms
+    if password_form.submit_password.data:
+        if password_form.validate():
+            hashed_pass = bcrypt.generate_password_hash(password_form.new_password.data).decode('utf-8')
+            current_user.password = hashed_pass
+            current_user.save()
+            flash("Your password has been updated!", category="success")
+            return redirect(url_for("users.account"))
+        else:
+            # Used to bring form into view using JS if there are validation errors
+            anchor = "update-password"
+
+    return render_template("account.html",
+                           title="Account",
+                           basic_info_form=basic_info_form,
+                           password_form=password_form,
+                           anchor=anchor)
